fd8507e392f0f7670213501a7017bfa19f6d1e04
BloodAvi <166813497+BloodAvi@users.noreply.github.com>
2024-06-30 17:54:29 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/bloodavi/kinguserbot /MissPerfectURLBOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 -m Branded
